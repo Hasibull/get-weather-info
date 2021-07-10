5521afde677b1f5775da7094a485b1ce0063bcbd
Hasibull <hasibulislam4500@gmail.com>
2021-07-10 21:27:12 +0600

    changing index.html and server.js

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const https = require('https');
 
 const app = express();
 
 app.get('/', function(request, response) {
-    response.send('Server is up and running!');
+    const userId = 'e9b9175e39b78f04323811ffcb7ed434';
+    const region = 'London';
+    const units = 'metric';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + region + '&units=' + units + '&appid=' + userId;
+    https.get(url, function(res) {
+        res.on('data', function(data) {
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+            response.send('The tempreture is = ' + weatherData.main.temp);
+        })
+    });
 });
 
 app.listen(3000, function() {
