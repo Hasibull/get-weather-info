f01e88a97a2051226a29c4e655c0b019ae38d862
Hasibull <hasibulislam4500@gmail.com>
2021-07-11 14:26:28 +0600

    final change in server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
+
 app.post('/', function(request, response) {
     const userId = 'e9b9175e39b78f04323811ffcb7ed434';
     const region = request.body.cityName;
@@ -17,9 +18,13 @@ app.post('/', function(request, response) {
     https.get(url, function(res) {
         res.on('data', function(data) {
             const weatherData = JSON.parse(data);
-            console.log(weatherData);
-            response.write('<div style="maring: 0; padding: 0;height: 100vh; width: 100vw; background-color: #4a4a4a; text-align: center;"><div style="width: 40%; padding: 20px; margin:0 auto; border: 10px inset yellow;"><p style="margin: 0; padding: 0; font-size: 25px; color: white;">The weather is currently ' + weatherData.weather[0].description + '</p>');
-            response.write('<h1>The Tempreture at ' + region + ' is ' + weatherData.main.temp + '</h1></div></div>')
+            const src = 'http://openweathermap.org/img/wn/' + weatherData.weather[0].icon + '@2x.png';
+
+            response.write('<body style="margin: 0; padding: 0">');
+            response.write('<div style="maring: 0; padding-top: 120px;height: 100%; width: 100%; background-color: #4a4a4a; text-align: center;"><div style="width: 50%; padding: 20px; margin: 0 auto; border: 10px inset yellow;"><p style="margin: 0; padding: 0; font-size: 25px; color: white;">The weather is currently ' + weatherData.weather[0].description + '</p>');
+            response.write('<h1 style="margin: 0; padding: 0; color: white; font-size: 45px;">The Tempreture at ' + region + ' is ' + weatherData.main.temp + ' degree</h1>');
+            response.write('<img src="' + src + '"' + '></div></div>')
+            response.write('</body>');
             response.send();
         })
     });
